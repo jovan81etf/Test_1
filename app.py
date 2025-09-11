ca070ea648aad4d02c74d3787049a26e0b0a26eb
jgalicetf <jovan.galic@etf.unibl.org>
2025-09-11 09:29:10 +0200

    Initial commit for render deployment

--- app.py
+++ app.py
@@ -123,6 +123,10 @@ def chart_page():
     ''')
 
 
+import os
+
 if __name__ == '__main__':
     init_db()
-    app.run(host='0.0.0.0', port=80)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
+
